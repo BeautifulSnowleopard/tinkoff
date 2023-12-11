0d875c075f7a3d48ac5ee8a2c4638ee4e1f052c5
DesignerMary <mmarshunina@yandex.ru>
2023-12-11 16:53:47 +0400

    Выволнила задания

--- task3/src/oop.js
+++ task3/src/oop.js
@@ -3,56 +3,69 @@
  * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
  * Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
  */
-class Point {
-    constructor(x = 0, y = 0) {
-      this.x = x;
-      this.y = y;
+class Point 
+{
+    constructor(x = 0, y = 0) 
+    {
+        this.x = x;
+        this.y = y;
     }
-  
-    dist() {
-      return Math.sqrt(this.x ** 2 + this.y ** 2);
-    }
-}
-  
-  /**
-   * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
-   * который будет наследоваться от точки в двумерном пространстве.
-   * Реализовать статический метод, который возвращает расстояние между Point3D.
-   */
-class Point3D extends Point {
-    constructor(x, y, z = 0) {
-      super(x, y);
-      this.z = z;
-    }
-  
-    static vectorLength(a, b) {
-      return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2) + Math.pow(b.z - a.z, 2));
+
+    findDistance() 
+    {
+        return Math.sqrt(x**2 + y**2);
     }
 }
-  
+
 /**
-   * Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала.
-   * Предусмотреть 2 варианта инициализации - массивом в конструкторе (из него создается очередь) и без параметров.
-   * Со звездочкой: написать тесты методы класса (oop.spec.js)
-   */
-class Queue {
-  
-    constructor(array = []) {
-      this.values = array;
+ * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
+ * который будет наследоваться от точки в двумерном пространстве.
+ * Реализовать статический метод, который возвращает расстояние между Point3D.
+ */
+class Point3D extends Point 
+{
+    constructor(x = 0, y = 0, z = 0) 
+    {
+        super(x, y);
+        this.z = z;
     }
-  
-    push(x) {
-      this.values.push(x);
-    }
-  
-    pop() {
-      if (!this.values.length) return false;
-      return this.values.shift();
+
+    static vectorLength(a, b) 
+    {
+        return Math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2);
     }
 }
-  
-module.exports = {
+
+/**
+ * Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала.
+ * Предусмотреть 2 варианта инициализации - массивом в конструкторе (из него создается очередь) и без параметров.
+ * Со звездочкой: написать тесты методы класса (oop.spec.js)
+ */
+class Queue {
+  constructor(arr = []) {
+      this.array = Array.isArray(arr) ? arr.slice() : [];
+  }
+
+  push(...elements) {
+      this.array.push(...elements);
+  }
+
+  pop() {
+      return this.array.length > 0 ? this.array.shift() : undefined;
+  }
+
+  get size() {
+      return this.array.length;
+  }
+
+  clear() {
+      this.array = [];
+  }
+}
+
+module.exports = 
+{
     Point,
     Point3D,
     Queue,
-  };
+};
